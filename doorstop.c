3fcd289452896ba7478a3f3c67a6278744b54347
ghorsington <neigh@coder.horse>
2020-05-23 11:36:37 -0700

    Fix not working on Unity 4 on MacOS

--- doorstop.c
+++ doorstop.c
@@ -30,19 +30,20 @@ extern void *_dl_sym(void *, const char *, void *);
 #define DYLD_INTERPOSE(_replacment, _replacee)
 #define INIT_DLSYM                                                 \
 {                                                                  \
-		if (real_dlsym == NULL)                                    \
-			real_dlsym = _dl_sym(RTLD_NEXT, "dlsym", dlsym_proxy); \
-		if (!strcmp(name, "dlsym"))                                \
-			return (void *)dlsym_proxy;                            \
+        if (real_dlsym == NULL)                                    \
+            real_dlsym = _dl_sym(RTLD_NEXT, "dlsym", dlsym_proxy); \
+        if (!strcmp(name, "dlsym"))                                \
+            return (void *)dlsym_proxy;                            \
 }
 #define INIT_FCLOSE                                                        \
 {                                                                          \
-		if (real_fclose == NULL)                                           \
-			real_fclose = real_dlsym(RTLD_NEXT, "fclose");                 \
+        if (real_fclose == NULL)                                           \
+            real_fclose = real_dlsym(RTLD_NEXT, "fclose");                 \
 }
 
 #elif __APPLE__
 #include <mach-o/dyld.h>
+#include "plthook.h"
 
 #define PATH_MAX 1024 // Maximum on macOS
 #define real_dlsym dlsym
@@ -50,27 +51,27 @@ extern void *_dl_sym(void *, const char *, void *);
 #define dlsym_proxy dlsym_proxy
 #define fclose_proxy fclose_proxy
 #define program_path(app_path)                    \
-	{                                             \
-		uint32_t bufsize = PATH_MAX;              \
-		_NSGetExecutablePath(app_path, &bufsize); \
-	}
+    {                                             \
+        uint32_t bufsize = PATH_MAX;              \
+        _NSGetExecutablePath(app_path, &bufsize); \
+    }
 #define DYLD_INTERPOSE(_replacment, _replacee) \
-	__attribute__((used)) static struct        \
-	{                                          \
-		const void *replacment;                \
-		const void *replacee;                  \
-	} _interpose_##_replacee                   \
-		__attribute__((section("__DATA,__interpose"))) = {(const void *)(unsigned long)&_replacment, (const void *)(unsigned long)&_replacee};
+    __attribute__((used)) static struct        \
+    {                                          \
+        const void *replacment;                \
+        const void *replacee;                  \
+    } _interpose_##_replacee                   \
+        __attribute__((section("__DATA,__interpose"))) = {(const void *)(unsigned long)&_replacment, (const void *)(unsigned long)&_replacee};
 #define INIT_DLSYM
 #define INIT_FCLOSE
 #endif
 
 // Set MonoArray's index to a reference type value (i.e. string)
 #define SET_ARRAY_REF(arr, index, refVal)                                            \
-	{                                                                                \
-		void **p = (void **)r_mono_array_addr_with_size(arr, sizeof(void *), index); \
-		r_mono_gc_wbarrier_set_arrayref(arr, p, refVal);                             \
-	}
+    {                                                                                \
+        void **p = (void **)r_mono_array_addr_with_size(arr, sizeof(void *), index); \
+        r_mono_gc_wbarrier_set_arrayref(arr, p, refVal);                             \
+    }
 
 void *(*r_mono_jit_init_version)(const char *root_domain_name, const char *runtime_version);
 void *(*r_mono_domain_assembly_open)(void *domain, const char *name);
@@ -99,149 +100,149 @@ void doorstop_init_mono_functions(void *handle)
 {
 #define LOAD_METHOD(m) r_##m = real_dlsym(handle, #m)
 
-	LOAD_METHOD(mono_jit_init_version);
-	LOAD_METHOD(mono_domain_assembly_open);
-	LOAD_METHOD(mono_assembly_get_image);
-	LOAD_METHOD(mono_runtime_invoke);
-	LOAD_METHOD(mono_method_desc_new);
-	LOAD_METHOD(mono_method_desc_search_in_image);
-	LOAD_METHOD(mono_method_signature);
-	LOAD_METHOD(mono_signature_get_param_count);
-	LOAD_METHOD(mono_array_new);
-	LOAD_METHOD(mono_gc_wbarrier_set_arrayref);
-	LOAD_METHOD(mono_array_addr_with_size);
-	LOAD_METHOD(mono_get_string_class);
-	LOAD_METHOD(mono_string_new);
-	LOAD_METHOD(mono_thread_current);
-	LOAD_METHOD(mono_thread_set_main);
-	LOAD_METHOD(mono_domain_set_config);
-	LOAD_METHOD(mono_assembly_getrootdir);
+    LOAD_METHOD(mono_jit_init_version);
+    LOAD_METHOD(mono_domain_assembly_open);
+    LOAD_METHOD(mono_assembly_get_image);
+    LOAD_METHOD(mono_runtime_invoke);
+    LOAD_METHOD(mono_method_desc_new);
+    LOAD_METHOD(mono_method_desc_search_in_image);
+    LOAD_METHOD(mono_method_signature);
+    LOAD_METHOD(mono_signature_get_param_count);
+    LOAD_METHOD(mono_array_new);
+    LOAD_METHOD(mono_gc_wbarrier_set_arrayref);
+    LOAD_METHOD(mono_array_addr_with_size);
+    LOAD_METHOD(mono_get_string_class);
+    LOAD_METHOD(mono_string_new);
+    LOAD_METHOD(mono_thread_current);
+    LOAD_METHOD(mono_thread_set_main);
+    LOAD_METHOD(mono_domain_set_config);
+    LOAD_METHOD(mono_assembly_getrootdir);
 
 #undef LOAD_METHOD
 }
 
 void *jit_init_hook(const char *root_domain_name, const char *runtime_version)
 {
-	// Call the original r_mono_jit_init_version to initialize the Unity Root Domain
-	void *domain = r_mono_jit_init_version(root_domain_name, runtime_version);
+    // Call the original r_mono_jit_init_version to initialize the Unity Root Domain
+    void *domain = r_mono_jit_init_version(root_domain_name, runtime_version);
 
-	if (strcmp(getenv("DOORSTOP_ENABLE"), "TRUE"))
-	{
-		printf("[Doorstop] DOORSTOP_ENABLE is not TRUE! Disabling Doorstop...\n");
-		return domain;
-	}
-	if (getenv("DOORSTOP_INITIALIZED"))
-	{
-		printf("DOORSTOP_INITIALIZED is set! Skipping!\n");
-		return domain;
-	}
-	setenv("DOORSTOP_INITIALIZED", "TRUE", TRUE);
+    if (strcmp(getenv("DOORSTOP_ENABLE"), "TRUE"))
+    {
+        printf("[Doorstop] DOORSTOP_ENABLE is not TRUE! Disabling Doorstop...\n");
+        return domain;
+    }
+    if (getenv("DOORSTOP_INITIALIZED"))
+    {
+        printf("DOORSTOP_INITIALIZED is set! Skipping!\n");
+        return domain;
+    }
+    setenv("DOORSTOP_INITIALIZED", "TRUE", TRUE);
 
-	if (r_mono_domain_set_config) 
-	{
-		char app_path[PATH_MAX] = "\0";
-		char config_path[PATH_MAX] = "\0";
-		program_path(app_path);
-		strcpy(config_path, app_path);
+    if (r_mono_domain_set_config) 
+    {
+        char app_path[PATH_MAX] = "\0";
+        char config_path[PATH_MAX] = "\0";
+        program_path(app_path);
+        strcpy(config_path, app_path);
 
-		strcat(config_path, ".config");
-		char *folder_path = dirname(app_path);
+        strcat(config_path, ".config");
+        char *folder_path = dirname(app_path);
 
-		printf("Setting config paths; basedir: %s; config: %s\n", folder_path, config_path);
-		r_mono_domain_set_config(domain, folder_path, config_path);
-	}
+        printf("Setting config paths; basedir: %s; config: %s\n", folder_path, config_path);
+        r_mono_domain_set_config(domain, folder_path, config_path);
+    }
 
-	char *dll_path = getenv("DOORSTOP_INVOKE_DLL_PATH");
+    char *dll_path = getenv("DOORSTOP_INVOKE_DLL_PATH");
 
-	char *assembly_dir = r_mono_assembly_getrootdir();
-	printf("Managed dir: %s\n", assembly_dir);
-	setenv("DOORSTOP_MANAGED_FOLDER_DIR", assembly_dir, TRUE);
-	free(assembly_dir);
+    char *assembly_dir = r_mono_assembly_getrootdir();
+    printf("Managed dir: %s\n", assembly_dir);
+    setenv("DOORSTOP_MANAGED_FOLDER_DIR", assembly_dir, TRUE);
+    free(assembly_dir);
 
-	// Load our custom assembly into the domain
-	void *assembly = r_mono_domain_assembly_open(domain, dll_path);
+    // Load our custom assembly into the domain
+    void *assembly = r_mono_domain_assembly_open(domain, dll_path);
 
-	if (assembly == NULL)
-	{
-		printf("Failed to load assembly\n");
-		return domain;
-	}
+    if (assembly == NULL)
+    {
+        printf("Failed to load assembly\n");
+        return domain;
+    }
 
-	// Get assembly's image that contains CIL code
-	void *image = r_mono_assembly_get_image(assembly);
+    // Get assembly's image that contains CIL code
+    void *image = r_mono_assembly_get_image(assembly);
 
-	printf("Got image: %p \n", image);
+    printf("Got image: %p \n", image);
 
-	if (image == NULL)
-	{
-		printf("Failed to locate the image!\n");
-		return domain;
-	}
+    if (image == NULL)
+    {
+        printf("Failed to locate the image!\n");
+        return domain;
+    }
 
-	// Note: we use the runtime_invoke route since jit_exec will not work on DLLs
+    // Note: we use the runtime_invoke route since jit_exec will not work on DLLs
 
-	// Create a descriptor for a random Main method
-	void *desc = r_mono_method_desc_new("*:Main", FALSE);
+    // Create a descriptor for a random Main method
+    void *desc = r_mono_method_desc_new("*:Main", FALSE);
 
-	// Find the first possible Main method in the assembly
-	void *method = r_mono_method_desc_search_in_image(desc, image);
+    // Find the first possible Main method in the assembly
+    void *method = r_mono_method_desc_search_in_image(desc, image);
 
-	if (method == NULL)
-	{
-		printf("Failed to locate any entrypoints!\n");
-		return domain;
-	}
+    if (method == NULL)
+    {
+        printf("Failed to locate any entrypoints!\n");
+        return domain;
+    }
 
-	void *signature = r_mono_method_signature(method);
+    void *signature = r_mono_method_signature(method);
 
-	// Get the number of parameters in the signature
-	uint32_t params = r_mono_signature_get_param_count(signature);
+    // Get the number of parameters in the signature
+    uint32_t params = r_mono_signature_get_param_count(signature);
 
-	void **args = NULL;
-	char app_path[PATH_MAX] = "\0";
-	if (params == 1)
-	{
-		// If there is a parameter, it's most likely a string[].
-		// Populate it as follows
-		// 0 => path to the game's executable
-		// 1 => --doorstop-invoke
+    void **args = NULL;
+    char app_path[PATH_MAX] = "\0";
+    if (params == 1)
+    {
+        // If there is a parameter, it's most likely a string[].
+        // Populate it as follows
+        // 0 => path to the game's executable
+        // 1 => --doorstop-invoke
 
-		program_path(app_path);
+        program_path(app_path);
 
-		void *exe_path = r_mono_string_new(domain, app_path);
-		void *doorstop_handle = r_mono_string_new(domain, "--doorstop-invoke");
+        void *exe_path = r_mono_string_new(domain, app_path);
+        void *doorstop_handle = r_mono_string_new(domain, "--doorstop-invoke");
 
-		void *args_array = r_mono_array_new(domain, r_mono_get_string_class(), 2);
+        void *args_array = r_mono_array_new(domain, r_mono_get_string_class(), 2);
 
-		SET_ARRAY_REF(args_array, 0, exe_path);
-		SET_ARRAY_REF(args_array, 1, doorstop_handle);
+        SET_ARRAY_REF(args_array, 0, exe_path);
+        SET_ARRAY_REF(args_array, 1, doorstop_handle);
 
-		args = malloc(sizeof(void *) * 1);
-		args[0] = args_array;
-	}
+        args = malloc(sizeof(void *) * 1);
+        args[0] = args_array;
+    }
 
-	r_mono_runtime_invoke(method, NULL, args, NULL);
+    r_mono_runtime_invoke(method, NULL, args, NULL);
 
-	if (args != NULL)
-	{
-		free(args);
-		args = NULL;
-	}
+    if (args != NULL)
+    {
+        free(args);
+        args = NULL;
+    }
 
-	return domain;
+    return domain;
 }
 
 void *dlsym_proxy(void *handle, const char *name)
 {
-	INIT_DLSYM;
+    INIT_DLSYM;
 
-	if (!strcmp(name, "mono_jit_init_version"))
-	{
-		doorstop_init_mono_functions(handle);
-		return (void *)jit_init_hook;
-	}
+    if (!strcmp(name, "mono_jit_init_version"))
+    {
+        doorstop_init_mono_functions(handle);
+        return (void *)jit_init_hook;
+    }
 
-	return real_dlsym(handle, name);
+    return real_dlsym(handle, name);
 }
 
 int fclose_proxy(FILE *stream) 
@@ -254,5 +255,41 @@ int fclose_proxy(FILE *stream)
 	return real_fclose(stream);
 }
 
+#if __APPLE__
+// Normal case: hook dlsym and get both handle to mono and hook to jit_init_version
 DYLD_INTERPOSE(dlsym_proxy, real_dlsym);
 DYLD_INTERPOSE(fclose_proxy, real_fclose);
+
+/*
+ On older Unity versions, Mono methods are resolved by the OS's loader directly.
+ Because of this, there is no dlsym, in which case we need to apply a PLT hook.
+*/
+
+void *find_mono_image()
+{
+    uint32_t cnt = _dyld_image_count();
+    for (uint32_t idx = 0; idx < cnt; idx++) 
+    {
+        const char *image_name = idx ? _dyld_get_image_name(idx) : NULL;
+        if (image_name && strstr(image_name, "libmono"))
+            return dlopen(image_name, RTLD_LAZY | RTLD_NOLOAD);
+    }
+    return NULL;
+}
+
+__attribute__((constructor)) void doorstop_main() 
+{
+    plthook_t *hook;
+
+    if(plthook_open(&hook, NULL) != 0)
+        printf("Failed to open current process PLT! err: %s\n", plthook_error());
+
+    if(plthook_replace(hook, "mono_jit_init_version", &jit_init_hook, NULL) == 0)
+    {
+        void *mono_handle = find_mono_image();
+        doorstop_init_mono_functions(mono_handle);
+    }
+    
+    plthook_close(hook);
+} 
+#endif
